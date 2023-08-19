1261c22da7069b2d8977ac22785ebcaecb233de0
Diyora Soyibjonova <dioora2o3@gmail.com>
2023-08-20 00:44:16 +0500

    add solution

--- 8kyu/Quarter of the year.js
+++ 8kyu/Quarter of the year.js
@@ -1,14 +1,15 @@
 const quarterOf = (month) => {
-  // Your code here
-  if (1 <= month >= 2) {
-    console.log("the first quarter");
-  } else if (3 <= month >= 5) {
-    console.log("the second quarter");
-  } else if (6 <= month >= 8) {
-    console.log("the third quarter");
-  } else if (9 <= month >= 11) {
-    console.log("the forth quarter");
+  if (month >= 1 && month <= 3) {
+    return 1;
+  } else if (month >= 4 && month <= 6) {
+    return 2;
+  } else if (month >= 7 && month <= 9) {
+    return 3;
+  } else if (month >= 10 && month <= 12) {
+    return 4;
+  } else {
+    throw new Error("Invalid month. Month must be between 1 and 12.");
   }
 };
-// console.log(quarterOf(6));
-quarterOf(8);
+
+console.log(quarterOf(5));
